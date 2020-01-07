b536ea1ce7200e8351e25cb19fa82d2d67bdbbd5
TatsianaH <ourflsun@gmail.com>
2020-01-07 00:53:04 -0500

    add nodemon.json add mongoose add mongoose.Schema

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+mongoose.connect('mongodb+srv://node-rest-shop:'+
+    process.env.MONGO_ATLAS_PASSWORD +
+    '@node-rest-shop-pq0pz.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true
+});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        res.status(200).json({});
+    }
+    next();
+});
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
