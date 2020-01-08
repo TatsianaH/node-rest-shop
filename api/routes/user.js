ef60e4b936eab07a8ea6f2209e9c71c82221dbf9
TatsianaH <ourflsun@gmail.com>
2020-01-07 20:14:10 -0500

    add user login and JWT signing delete nodemon.json

--- api/routes/user.js
+++ api/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
 const bcrypt = require('bcrypt');
-
+const jwt = require('jsonwebtoken');
 
 router.post('/signup', (req, res, next) => {
     User.find({email: req.body.email})
@@ -44,8 +44,55 @@ router.post('/signup', (req, res, next) => {
                             });
                     }
                 });
-}
+            }
+        });
 });
+
+router.post('/login', (req, res, next) => {
+    User.find({email: req.body.email})
+        .exec()
+        .then( user => {
+            if(user.length < 1){
+                return res.status(401).json({
+                    message: 'Auth failed. Number of users is less than 1'
+                });
+            }
+            bcrypt.compare(req.body.password, user[0].password,  (err, result) => {
+                if(err){
+                    return res.status(401).json({
+                        message: 'Auth failed.'
+                    })
+                }
+                if(result){
+                    const token = jwt.sign({
+                        email: user[0].email,
+                        userId: user[0]._id
+                    },
+                        process.env.JWT_KEY,
+                        {
+                            expiresIn: "1h"
+                        }
+                        );
+                    return res.status(200).json({
+                        message: 'Auth successful',
+                        payload: {
+                            email: user[0].email,
+                            userId: user[0]._id,
+                            token: token,
+                        }
+                    });
+                }
+                res.status(401).json({
+                    message: 'Auth failed. Password'
+                });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 router.delete('/:userId', (req, res, next) => {
     User.deleteOne({_id: req.params.userId})
